e22599b7818109f3b837ca82156951d0eeb3edf6
drvmukhin <dr.mukhin@gmail.com>
2022-05-20 22:58:26 -0700

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 setuptools.setup(name='pyquark',
-version='0.1',
+version='0.2.0',
 description='Helper utilities',
-url='#',
+url='https://github.com/drvmukhin/pyquark.git',
 author='vmukhin.dev',
 install_requires=[],
 author_email='vmukhin.dev@gmail.com',
+license='MIT',
 packages=setuptools.find_packages(),
+dependency_links=[],
 zip_safe=False)
