b1784a363c3ceca7a43aa7388ad5e71a448abf62
drvmukhin <dr.mukhin@gmail.com>
2022-05-20 22:32:38 -0700

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='pyquark',
+version='0.1',
+description='Helper utilities',
+url='#',
+author='vmukhin.dev',
+install_requires=[],
+author_email='vmukhin.dev@gmail.com',
+packages=setuptools.find_packages(),
+zip_safe=False)
